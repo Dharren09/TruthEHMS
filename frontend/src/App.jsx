a8b93f875e55ae96899b1e2dbf9a5ed8a843a6bd
Dharren09 <dharrydharrenzerah@gmail.com>
2023-09-19 20:51:24 +0300

    initial set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-[25px] text-blue-500">Hello, welcome to my Vite + React app</h1>
+    </>
+  )
+}
+
+export default App
